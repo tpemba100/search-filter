a35da6f048e7ff58450ca960d993645c26b38da4
tpemba100 <tpemba.96@gmail.com>
2022-12-26 18:38:30 -0500

    added the app.js and created a mock list data using Mockoroo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { data } from "./data.js";
 
 function App() {
+  console.log(data);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="text-center mt-4">Contact Page</h1>
+        <Form>
+          <InputGroup className="my-3">
+            <Form.Control placeholder="Search Contact" />
+          </InputGroup>
+        </Form>
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Luke</td>
+              <td>Sky</td>
+              <td>email@gmail.com</td>
+              <td>123-223-4424</td>
+            </tr>
+          </tbody>
+
+          {/* <tbody>
+            {data
+              .filter((item) => {
+                return search.toLowerCase() === ''
+                  ? item
+                  : item.first_name.toLowerCase().includes(search);
+              })
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              ))}
+          </tbody> */}
+        </Table>
+      </Container>
     </div>
   );
 }
